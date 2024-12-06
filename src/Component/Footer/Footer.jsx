dd489a82cb804f059df61904626a33824a1cb550
MD JIHAD <admin@gmail.com>
2024-12-06 12:52:41 +0600

    error page complete

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="px-4 divide-y text-gray-800 relative bottom-0 left-0">
+    <footer className="px-4 divide-y text-gray-800 relative bottom-0 left-0 mt-6 lg:my-2">
       <div className="py-6 text-sm text-center text-gray-400 font-semibold">
         © 2023 nextPage Inc. All rights reserved.
       </div>
